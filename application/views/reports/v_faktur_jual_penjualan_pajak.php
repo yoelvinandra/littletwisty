fd8e91d0022f10344a093be65959fcbfb7857380
yoelvinandra <yoelvinandra@gmail.com>
2025-11-20 11:01:58 +0700

    UPDATE NO REK INVOICE

--- application/views/reports/v_faktur_jual_penjualan_pajak.php
+++ application/views/reports/v_faktur_jual_penjualan_pajak.php
@@ -427,7 +427,7 @@ if($rp->NPWP != null){ $npwp_perusahaan = 'NPWP. '.$rp->NPWP;}
     				
         				<table width="100%" style="margin-top:2px;">
         					<tr valign="top">
-        					<td class="font-body " colspan="3"><b>Bank Transfer</b><br>UOB 303-303-590-8<br>KARATU ABADI JAYA,CV</td>
+        					<td class="font-body " colspan="3"><b>Bank Transfer</b><br>BCA 8290883106<br>KARATU ABADI JAYA CV</td>
         					</tr>
         				</table>
     				</td>
